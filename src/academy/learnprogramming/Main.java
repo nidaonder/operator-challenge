350690fee69d0184bdecb6c82c2c00df6ddb9318
nida önder <nidaaonder@gmail.com>
2022-09-27 22:12:14 +0300

    operators

---
+++ src/academy/learnprogramming/Main.java
@@ -0,0 +1,18 @@
+package academy.learnprogramming;
+
+public class Main {
+    public static void main (String [] args) {
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue ) * 100d;
+            System.out.println("myValuesTotal = " + myValuesTotal);
+        double theRemainder = (myValuesTotal % 40.00d);
+            System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false ;
+            System.out.println("isNoRemainder = " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder.");
+        }
+
+    }
+}
